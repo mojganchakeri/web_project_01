ac85eccdcf87cc0cb3e6fd8445c8a06635c44040
Mojgan Chakeri <mojgan1995@gmail.com>
2016-10-19 17:32:20 +0330

    add some front-end files,replace bootstrap template, complete rotues section in app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -22,8 +21,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+/*
+  |---------------------------------
+  |         Routes Section
+  |---------------------------------
+*/
+
+app.use('/', require('./routes/index'));
+app.use('/panel',require('./routes/panel'));
+app.use('/admin',require('./routes/admin'));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page_title: 'Express' });
 });
+
+/* Get Login Page */
+router.get('/login',function(req,res,next){
+	res.render('auth/login',{page_title : 'Login'});
+});
+
+/* Get Register Page */
+router.get('/register',function(req,res,next){
+	res.render('auth/register',{page_title : 'Register'});
+});
+
+
+
+router.post('/login',function(req,res,next){
+	//TODO complete login post page
+	res.render('auth/login',{page_title:'Login'});
+});
+
+router.post('/register',function(req,res,next){
+	//TODO complete register post page
+	res.render('auth/register',{page_title :'Register'});
+});
+
 
 module.exports = router;
